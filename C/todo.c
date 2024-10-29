75d1f7d99d2958c99de80655cb60f4b2942ba3bf
Powopaf <paf.fournier@gmail.com>
2024-10-29 11:26:29 +0100

    add function to write the task in a file

--- C/todo.c
+++ C/todo.c
@@ -8,24 +8,20 @@ void add(char* task, char* day, char* month, char* year) {
     // FORMAT :
     //\ntask day month year
     FILE* file = fopen("data", "a+");
-    size_t length = strlen(task) + strlen(day) + strlen(month) + strlen(year) + 5;
+    size_t length = strlen(task) +strlen(day) + strlen(month) + strlen(year) + 5;
     char* task_to_add = malloc(sizeof(char) * length);
-    task_to_add[0] = '\n';
-    strcat(task_to_add, task);
-    task_to_add[strlen(task) + 1] = ' ';
-    strcat(task_to_add, day);
-    task_to_add[strlen(task) + strlen(day) + 2] = ' ';
-    strcat(task_to_add, month);
-    task_to_add[strlen(task) + strlen(day) + strlen(month) + 3] = ' ';
-    strcat(task_to_add, year);
+    sprintf(task_to_add, "%s %s %s %s\n", task, day, month, year);
     fprintf(file, task_to_add);
+    free(task_to_add);
+    printf("Task: %s was successfuly added.\n", task);
 }
 
 int main(int argc, char* argv[]) {
-    if (strcmp(argv[1], "add") && argc == 6) {
+    if (strcmp(argv[1], "add") == 0 && argc == 6) {
         // TODO : make sure argv[2,3,4,5] are valid
         add(argv[2], argv[3], argv[4], argv[5]);
         return EXIT_SUCCESS;
     }
+    printf("error\n");
     return EXIT_FAILURE;
 }
