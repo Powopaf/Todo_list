e3b03ba97f17bf72a3ca9332c39887eef450aa27
Powopaf <paf.fournier@gmail.com>
2024-10-29 12:07:57 +0100

    finish error handling in todo.c

--- C/todo.c
+++ C/todo.c
@@ -3,24 +3,41 @@
 #include <string.h>
 #include <stdlib.h>
 
-void add(char* task, char* day, char* month, char* year) {
+const char* task_filename = "data"; // if task_filename is change put the new
+                                    // name in the .gitignore file
+                                    // file is create if it do not exist
+
+int add(char* task, char* day, char* month, char* year) {
     // write in the data file the new task to add
     // FORMAT :
     //\ntask day month year
-    FILE* file = fopen("data", "a+");
+
+    FILE* file = fopen(task_filename, "a+");
+    if (file == NULL) { printf("\033[0;31mFailed to open file data (fopen() error)\033[0m\n"); return EXIT_FAILURE; }
+    
     size_t length = strlen(task) +strlen(day) + strlen(month) + strlen(year) + 5;
     char* task_to_add = malloc(sizeof(char) * length);
-    sprintf(task_to_add, "%s %s %s %s\n", task, day, month, year);
-    fprintf(file, task_to_add);
+    if (task_to_add == NULL) { printf("\033[0;31mFailed To add task %s (malloc failure)\033[0m\n", task); return EXIT_FAILURE; }
+
+    if (sprintf(task_to_add, "%s %s %s %s\n", task, day, month, year) < 0) { printf("\033[0;31mFail to build string with sprintf()\033[0m\n"); return EXIT_FAILURE; }
+    
+    if (fprintf(file, task_to_add) < 0) { printf("\033[0;31mFail To write in file (fprintf() error\033[0m\n"); return EXIT_FAILURE; }
+    
     free(task_to_add);
     printf("Task: %s was successfuly added.\n", task);
+    return EXIT_SUCCESS;
+}
+
+int help() {
+    const char* help_add = "To add a new task the argument must be :\n\033[0;35madd\033[0m \033[0;34mtask day month year\033[0m";
+    return EXIT_SUCCESS;
 }
 
 int main(int argc, char* argv[]) {
+    printf("%s\n", help_add);
     if (strcmp(argv[1], "add") == 0 && argc == 6) {
         // TODO : make sure argv[2,3,4,5] are valid
-        add(argv[2], argv[3], argv[4], argv[5]);
-        return EXIT_SUCCESS;
+        return add(argv[2], argv[3], argv[4], argv[5]);
     }
     printf("error\n");
     return EXIT_FAILURE;
