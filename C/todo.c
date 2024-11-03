4d9cbb34df01a7cf8d545e4b32dbfcaf74379ec4
Powopaf <paf.fournier@gmail.com>
2024-11-03 12:07:14 +0100

    add help but issue when trying to display it

--- C/todo.c
+++ C/todo.c
@@ -38,6 +38,7 @@ static int add(char* task, char* day, char* month, char* year) {
     }
     
     free(task_to_add);
+    fclose(file);
     printf("Task: %s was successfuly added.\n", task);
     return EXIT_SUCCESS;
 }
@@ -47,15 +48,23 @@ static int help(char display) {
      * diplay   = a -> print ONLY add help
      *          = \0 -> print all help possible
     */
-    static char* help_add = "To add a new task the argument must be :\n\033[0;35madd\033[0m \033[0;34mtask day month year\033[0m";
+    static char* g1 = "Bellow are the command you you can use for add and delete task\n";
+    char* g3 = "\033[35mhelp \033[0m\033[34del\033[0m to display add help only\n\033[35help\033[0m \033[34mdel\033[0m to diplay delete help onlyi\n";
+    char* g2 = "Note that if no task where ever added ther will be error when printing task or deleting them\n";
+    static char* help_add = "To add a new task the argument must be :\n\033[0;35madd\033[0m \033[0;34mtask day month year\033[0m\n";
+    static char* help_del = "To delete a new task the do :\n\033[35mdel \033[0m\033[34mtask_name\033[0m\n";
     if (display == 'a') {
-        printf("%s\n", help_add);
+        printf("%s", help_add);
+        return EXIT_SUCCESS;
+    }
+    else if (display == 'd') {
+        printf("%s", help_del);
         return EXIT_SUCCESS;
     }
     else {
-        char* list_help[1] = { help_add };
-        for (size_t i = 0; i < 1; i++) {
-            printf("%s\n", list_help[i]);
+        char* list_help[5] = { g1, g2, g3, help_add, help_del };
+        for (size_t i = 0; i < 5; i++) {
+            printf("%s", list_help[i]);
         }
     return EXIT_SUCCESS;
     }
@@ -85,6 +94,26 @@ static int read_data() {
         printf("%s", to_print);
         free(to_print);
     }
+    fclose(file);
+    return EXIT_SUCCESS;
+}
+
+static int delete_task(char* task) {
+    FILE *file = fopen(task_filename, "r"); // file must exist
+    if (file == NULL) { return EXIT_FAILURE; }
+    FILE *tempfile = fopen("t_data", "w");
+    char line[128];
+    while (fgets(line, 128, file)) {
+        char f_task[118];
+        sscanf(line, "%s", f_task);
+        if (strcmp(task, f_task) != 0) {
+            fputs(line, tempfile);
+        }
+    }
+    fclose(file);
+    fclose(tempfile);
+    remove(task_filename);
+    rename("t_data", task_filename);
     return EXIT_SUCCESS;
 }
 
@@ -96,8 +125,13 @@ int main(int argc, char* argv[]) {
     else if (strcmp(argv[1], "add") == 0 && argc == 6 && check_date(argv[3], argv[4], argv[5]) == 'T') {
         return add(argv[2], argv[3], argv[4], argv[5]);
     }
-
-    else {
-        return help('\0');
+    else if (strcmp(argv[1], "del") == 0 && argc >= 2) {
+            return delete_task(argv[2]);
     }
+    else if (strcmp(argv[1], "help") == 0 && argc == 2){
+        if (strcmp(argv[2], "add") == 0) { return help('a'); }
+        else if (strcmp(argv[2], "del") == 0) { return help('d'); }
+        else { return help('\0'); }
+    }
+    else { return help('\0'); }
 }
