efed50c9b2ab226897a0660d060fce783dc923e9
Powopaf <paf.fournier@gmail.com>
2024-11-04 14:56:24 +0100

    add function to add task

--- Rust/todo/src/main.rs
+++ Rust/todo/src/main.rs
@@ -1,8 +1,16 @@
 use std::env;
+use std::fs::OpenOptions;
+use std::io::Error;
+use std::io::Write;
 
-fn add_task(task: &str, day: &str, month: &str, year: &str) -> () {
+fn add_task(task: &str, day: &str, month: &str, year: &str) -> Result<(), Error> {
+    let file_name = "data";
+    let file = OpenOptions::new().append(true).open(file_name);
+    let line = task.to_owned() + " " + day + " " + month + " " + year + "\n";
+    file?.write_all(line.as_bytes())?;
     println!("Add new task {0} that must be done before {1} {2} {3}",
              task, day, month, year);
+    Ok(())
 }
 
 fn main() {
@@ -11,7 +19,7 @@ fn main() {
         println!("Display Task");
         }
     else if args.len() == 6 && args[1].eq("add") {
-        add_task(&args[2], &args[3], &args[4], &args[5]); 
+        let _ = add_task(&args[2], &args[3], &args[4], &args[5]); 
     }
     else if args[1].eq("del") {
         println!("Delete task");
